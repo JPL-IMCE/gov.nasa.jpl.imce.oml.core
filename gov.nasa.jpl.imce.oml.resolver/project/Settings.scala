5433b4d7198dcfeb9f9d796b78266b0fdd4f3d25
Nicolas F Rouquette <nicolas.f.rouquette@jpl.nasa.gov>
2017-03-09 07:48:34 -0800

    git subrepo clone git@github.com:JPL-IMCE/gov.nasa.jpl.imce.oml.resolver.git gov.nasa.jpl.imce.oml.resolver
    subrepo:
      subdir:   "gov.nasa.jpl.imce.oml.resolver"
      merged:   "42a2eb6"
    upstream:
      origin:   "git@github.com:JPL-IMCE/gov.nasa.jpl.imce.oml.resolver.git"
      branch:   "master"
      commit:   "42a2eb6"
    git-subrepo:
      version:  "0.3.1"
      origin:   "git@github.com:ingydotnet/git-subrepo.git"
      commit:   "55f8cfd"

---
+++ gov.nasa.jpl.imce.oml.resolver/project/Settings.scala
@@ -0,0 +1,60 @@
+import sbt._
+import Keys._
+import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
+
+object Settings {
+
+  val name = "gov.nasa.jpl.imce.omf.resolver"
+
+  val namespace = "gov.nasa.jpl.imce.omf.resolver"
+
+  val organization = "gov.nasa.jpl.imce"
+
+  val version = "0.19.0"
+
+  val scalacOptions = Seq(
+    "-deprecation",
+    "-encoding", "UTF-8",     // yes, this is 2 args
+    "-feature",
+    "-language:existentials",
+    "-language:higherKinds",
+    "-language:implicitConversions",
+    "-unchecked",
+    "-Xfatal-warnings",
+    "-Xlint",
+    "-Yno-adapted-args",
+    "-Ywarn-dead-code",       // N.B. doesn't work well with the ??? hole
+    "-Ywarn-numeric-widen",
+    "-Ywarn-value-discard",
+    "-Xfuture",
+    "-Ywarn-unused-import",   // 2.11 only
+    "-Yno-imports"            // no automatic imports at all; all symbols must be imported explicitly
+  )
+
+  object versions {
+    val jpl_omf_schema_tables = "0.49.+"
+    val scala = "2.11.8"
+    val scalaJ8CompatVersion = "0.8.0-RC8"
+    val scalaGraphLibraries = "3.4.+"
+  }
+
+  /**
+    * These dependencies are shared between JS and JVM projects
+    * the special %%% function selects the correct version for each project
+    */
+  val sharedDependencies = Def.setting(Seq(
+  ))
+
+
+  /** Dependencies only used by the JVM project */
+  val jvmDependencies = Def.setting(Seq(
+    "gov.nasa.jpl.imce" %% "gov.nasa.jpl.imce.oml.tables" % versions.jpl_omf_schema_tables
+  ))
+
+
+  /** Dependencies only used by the JS project (note the use of %%% instead of %%) */
+  val scalajsDependencies = Def.setting(Seq(
+    "gov.nasa.jpl.imce" %%% "gov.nasa.jpl.imce.oml.tables" % versions.jpl_omf_schema_tables
+  ))
+
+}
